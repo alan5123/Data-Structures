c680e052ca3ca760726b8cd68249b0fa4a498ff0
alan5123 <alanparker5123@gmail.com>
2020-07-14 19:00:50 -0400

    mvp

--- stack/singly_linked_list.py
+++ stack/singly_linked_list.py
@@ -52,23 +52,28 @@ class LinkedList:
         # set self.head to the Node after the head 
         self.head = self.head.get_next()
         return val
+
+
     def remove_tail(self):
-        # if we have an empty linked list 
-        if self.head is None and self.tail is None:
-            return
-        # if we have a non-empty linked list 
-        # we have to start at the head and move down the linked list 
-        # until we get to the node right before the tail 
-        # iterate over our linked list 
-        current = self.head 
+        if self.head is None:
+            return 
+
+        if self.head is self.tail:
+            value = self.head.get_value()
+            self.head = None
+            self.tail = None
+            return value
+        
+        current = self.head
+
         while current.get_next() is not self.tail:
             current = current.get_next()
-        # at this point, `current` is the node right before the tail 
-        # set the tail to be None
-        val = self.tail.get_value() 
-        # move self.tail to the Node right before
+
+        value = self.tail.get_value()
         self.tail = current
-        return val
+        self.tail.set_next(None)
+        return value
+        
     def contains(self, value):
         if not self.head:
             return False
